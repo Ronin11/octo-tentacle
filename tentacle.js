1f6fd3d6432b9b72fc8f38d794326d8904175dec
Ronin11 <nate@raddd.com>
2017-07-16 16:04:11 -0600

    Stuff

--- tentacle.js
+++ tentacle.js
@@ -24,6 +24,6 @@
 // });
 
 const io = require('socket.io-client');
-const socket = io('http://localhost:8080');
+const socket = io('http://35.185.250.232/');
 
 socket.emit('chat message', "HIIIII!!!");
