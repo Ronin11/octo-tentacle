68c591a45f5828850e5b88b9bb84090c47b7af45
Nate Ashby <nate.ashby11@gmail.com>
2018-07-31 17:34:25 -0600

    OOOFFF

--- mantle.go
+++ mantle.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	// "time"
+	"time"
 	"os/signal"
 	"syscall"
 
@@ -16,24 +16,35 @@ func main() {
 	fmt.Println("\n~~~~~~~ Starting App ~~~~~~~")
 
 	network := octo.JoinNetwork(os.Getenv("SERVER"), octo.NATSNetwork)
-	// err := octo.CreateNatsListener(network.GetServerAddress(), func(message string, subject string) {
-	// 	fmt.Printf("Subject: %s \tMessage: %s\n", subject, message)
-	// })
-	// if err != nil{
-	// 	panic(err)
-	// }
+	err := octo.CreateNatsListener(network.GetServerAddress(), func(message string, subject string) {
+		fmt.Printf("Subject: %s \tMessage: %s\n", subject, message)
+	})
+	if err != nil{
+		panic(err)
+	}
 
+	sprinklerService.CreateService(network)
+	sprinklerService.CreateService(network)
+	sprinklerService.CreateService(network)
+	sprinklerService.CreateService(network)
+	sprinklerService.CreateService(network)
 	sprinklerService.CreateService(network)
 
 
-	// messenger := octo.CreateMessenger("discovery", network)
-	// go func(){
-	// 	duration := time.Second * 5
-	// 	time.Sleep(duration)
-	// 	messenger.WriteAndListen("?", func(msg string){
-	// 		fmt.Println("asdf: ", msg)
-	// 	})
-	// }()
+	messenger := octo.CreateMessenger("discovery", network)
+	go func(){
+		duration := time.Second
+		time.Sleep(duration)
+		var services []string
+		messenger.Subscribe(func(message string){
+			if message != "?" {
+				services = append(services, message)
+			}
+		})
+		messenger.Write("?")
+		time.Sleep(duration * 5)
+		fmt.Println("Available Services: ", services)
+	}()
 
 	exitSignal := make(chan os.Signal)
 	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
--- services/sprinkler/sprinkler.go
+++ services/sprinkler/sprinkler.go
@@ -4,10 +4,11 @@ import (
 	"time"
 	"log"
 	"fmt"
+	"encoding/json"
 
 	"github.com/octo-tentacle/pkg/octo"
 
-	"github.com/Ronin11/go-rpio"
+	// "github.com/Ronin11/go-rpio"
 )
 
 type sprinklerService struct {
@@ -16,10 +17,10 @@ type sprinklerService struct {
 }
 
 type sprinklerData struct {
-	sprinklerIsOn bool
+	SprinklerIsOn bool `json:"sprinklerIsOn"`
 }
 
-var config octo.Config
+var config *octo.Config
 var service sprinklerService
 
 // CreateService ...
@@ -30,49 +31,115 @@ func CreateService(network *octo.Network) octo.Service{
 			Write: true,
 		},
 		data: sprinklerData{
-			sprinklerIsOn: false,
+			SprinklerIsOn: false,
 		},
 	}
-
-	config, err := octo.ReadConfigFile("./services/sprinkler/config.json")
+	var id = 0
+	var err error
+	config, err = octo.ReadConfigFile("./services/sprinkler/config.json")
 	if err != nil{
 		log.Fatal(err)
 	}
 
+	idMessenger := octo.CreateMessenger(
+		fmt.Sprintf("%s.discovery", config.Service), 
+		network,
+	)
+	var services []string
+	idMessenger.Subscribe(func(message string){
+		if message != "?" {
+			services = append(services, message)
+		}
+	})
+	idMessenger.Write("?")
+	time.Sleep(time.Second * 2)
+	if len(services) > 0 {
+		id = len(services)
+	}
+
+	
 	for _, channel := range config.OutputChannels {
-		messenger := octo.CreateMessenger(channel.Name, network)
+		messenger := octo.CreateMessenger(
+			fmt.Sprintf("%s.%s.%d.%s", config.Service, config.Group, id, channel.Name), 
+			network,
+		)
 		startServiceWriter(messenger)
 	}
 
+	for _, channelName := range config.InputChannels {
+		messenger := octo.CreateMessenger(
+			fmt.Sprintf("%s.%s.%d.%s", config.Service, config.Group, id, channelName),
+			network,
+		)
+		startServiceListener(messenger)
+
+		// go func(){
+		// 	for i := 0; true; i++ {
+		// 		if i % 2 == 0 {
+		// 			messenger.Write(`{"sprinklerIsOn":true}`)
+		// 		} else {
+		// 			messenger.Write(`{"sprinklerIsOn":false}`)
+		// 		}
+		// 		duration := time.Second
+		// 		time.Sleep(duration)
+		// 	}
+		// }()
+	}
+	serviceDiscoveryMessenger := octo.CreateMessenger(
+		fmt.Sprintf("%s.discovery", config.Service),
+		network,
+	)
+	serviceDiscoveryMessenger.Subscribe(func(message string){
+		if(message == "?"){
+			serviceDiscoveryMessenger.Write(
+				fmt.Sprintf("%s.%s.%d:%+v", config.Service, config.Group, id, service.data),
+			)
+		}
+	})
 	discoveryMessenger := octo.CreateMessenger("discovery", network)
-	startServiceDiscoveryListener(discoveryMessenger)
+	discoveryMessenger.Subscribe(func(message string){
+		if(message == "?"){
+			discoveryMessenger.Write(
+				fmt.Sprintf("%s.%s.%d:%+v", config.Service, config.Group, id, service.data),
+			)
+		}
+	})
 	
 	// go serviceLogic(config)
 	return &service
 }
 
-func startServiceDiscoveryListener(messenger octo.Messenger){
-	messenger.Subscribe(func(message string){
-		if(message == "?"){
-			messenger.Write(fmt.Sprintf("%+v", service.data))
-		}
-	})
-}
-
+const okMessage = "Ok"
 func startServiceListener(messenger octo.Messenger){
 	messenger.Subscribe(func(message string){
-		// OnMessage(message)
+		if message != okMessage {
+			messenger.Write(OnMessage(message))
+		}
 	})
 }
 
 func startServiceWriter(messenger octo.Messenger){
 	go func(){
 		for i := 0; true; i++ {
-			messenger.Write(fmt.Sprintf("%+v", service.data))
+			data, err := json.Marshal(service.data)
+			if err != nil {
+				log.Fatal(err)
+			}
+			messenger.Write(string(data))
 			duration := time.Second
   		time.Sleep(duration)
 		}
 	}()
+}
+
+func OnMessage(message string) string{
+	var temp sprinklerData
+	err := json.Unmarshal([]byte(message), &temp)
+	if err != nil {
+		return err.Error()
+	}
+	service.data = temp
+	return okMessage
 }
 
 func serviceLogic(messenger octo.Messenger){
@@ -80,36 +147,29 @@ func serviceLogic(messenger octo.Messenger){
 	// 	data.sprinklerIsOn = !data.sprinklerIsOn
 	// 	time.Sleep(time.Second * 5)
 	// }
-	const pin = rpio.Pin(18)
-	go func(){
-		// Open and map memory to access gpio, check for errors
-		if err := rpio.Open(); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-		}
- 		// Set pin to output mode
-		pin.Output()
-		// Unmap gpio memory when done
-		defer rpio.Close()
- 		for i := 0; true; i++ {
-			messenger.Write(fmt.Sprintf("%+v", service.data))
-			oof := i%2
-			if(oof > 0){
-				pin.Write(rpio.High)
-			}else{
-				pin.Write(rpio.Low)
-			}
-			messenger.Write(fmt.Sprintf("%+v", service.data))
-			duration := time.Second
-  			time.Sleep(duration)
-		}
- 	}()
+
+	// const pin = rpio.Pin(18)
+	// go func(){
+	// 	// Open and map memory to access gpio, check for errors
+	// 	if err := rpio.Open(); err != nil {
+	// 			fmt.Println(err)
+	// 			os.Exit(1)
+	// 	}
+ 	// 	// Set pin to output mode
+	// 	pin.Output()
+	// 	// Unmap gpio memory when done
+	// 	defer rpio.Close()
+ 	// 	for i := 0; true; i++ {
+	// 		messenger.Write(fmt.Sprintf("%+v", service.data))
+	// 		oof := i%2
+	// 		if(oof > 0){
+	// 			pin.Write(rpio.High)
+	// 		}else{
+	// 			pin.Write(rpio.Low)
+	// 		}
+	// 		messenger.Write(fmt.Sprintf("%+v", service.data))
+	// 		duration := time.Second
+  // 			time.Sleep(duration)
+	// 	}
+ 	// }()
 }
-
-// func (s sprinklerService) Init(){
-
-// }
-
-// func (s sprinklerService) OnMessage(message string){
-// 	fmt.Println("MSG: ", message)
-// }
